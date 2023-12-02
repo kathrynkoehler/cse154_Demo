f44918607133735515585398e3fb3c6fbea8f047
eliasbelzberg <ebelz@uw.edu>
2023-12-01 19:00:10 -0800

    live push2

---
+++ personal.py
@@ -0,0 +1,92 @@
+import streamlit as st
+import requests
+from streamlit_lottie import st_lottie
+
+
+st.set_page_config(page_title="Cool website" ,page_icon = ":fire:", layout="wide")
+
+def lottie_request(url):
+  res = requests.get(url)
+  if res.status_code != 200:
+    return None
+  return res.json()
+
+lottie_animation = lottie_request("https://lottie.host/063967a2-9475-494a-b25e-4be2af9e3d6f/z4iD50gHm8.json")
+
+st.header("Hi, Welcome to my website :wave:")
+st.divider()
+
+# About Me
+st.header("About Me")
+column1, column2 = st.columns(2)
+with column1:
+  st.write("Hi, my name is Elias I am a Senior at the University of Washington!")
+  st.markdown("""
+              #### Classes I am taking
+              - cse154
+              - cse154
+              - cse154
+              """)
+  st.write(
+    """
+    Commodo sed egestas egestas fringilla phasellus faucibus scelerisque. Pulvinar mattis nunc sed blandit libero volutpat sed cras. Fringilla est ullamcorper eget nulla. Egestas sed tempus urna et pharetra pharetra massa massa. Nunc vel risus commodo viverra maecenas accumsan lacus. Justo donec enim diam vulputate ut pharetra. Metus aliquam eleifend mi in nulla posuere sollicitudin. Lobortis mattis aliquam faucibus purus. Massa tincidunt dui ut ornare lectus sit amet. Et molestie ac feugiat sed lectus vestibulum. Mattis ullamcorper velit sed ullamcorper. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Pulvinar sapien et ligula ullamcorper malesuada. Orci phasellus egestas tellus rutrum tellus pellentesque. Ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet. Etiam erat velit scelerisque in dictum non consectetur.
+    """
+  )
+
+with column2:
+  st_lottie(lottie_animation, height=400)
+
+
+# Projects
+st.divider()
+with st.container():
+  st.header("Projects")
+  img_column, text_column = st.columns((1,2))
+  with img_column:
+    st.image("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fd2%2Ffa%2F99%2Fd2fa99e4c66da9df17f40474b62864b1.jpg&f=1&nofb=1&ipt=775293a832086fafb85491ec1d083db04c1457f052fa56baaa88f50a1ff2834b&ipo=imagess")
+  with text_column:
+    st.subheader("My dubhacks project")
+    st.write(
+    """
+    Commodo sed egestas egestas fringilla phasellus faucibus scelerisque. Pulvinar mattis nunc sed blandit libero volutpat sed cras. Fringilla est ullamcorper eget nulla. Egestas sed tempus urna et pharetra pharetra massa massa. Nunc vel risus commodo viverra maecenas accumsan lacus. Justo donec enim diam vulputate ut pharetra. Metus aliquam eleifend mi in nulla posuere sollicitudin. Lobortis mattis aliquam faucibus purus. Massa tincidunt dui ut ornare lectus sit amet. Et molestie ac feugiat sed lectus vestibulum. Mattis ullamcorper velit sed ullamcorper. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Pulvinar sapien et ligula ullamcorper malesuada. Orci phasellus egestas tellus rutrum tellus pellentesque. Ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet. Etiam erat velit scelerisque in dictum non consectetur.
+    """
+    )
+    st.markdown("[Link](www.example.com)")
+with st.container():
+  st.header("Projects")
+  img_column, text_column = st.columns((1,2))
+  with img_column:
+    st.image("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fd2%2Ffa%2F99%2Fd2fa99e4c66da9df17f40474b62864b1.jpg&f=1&nofb=1&ipt=775293a832086fafb85491ec1d083db04c1457f052fa56baaa88f50a1ff2834b&ipo=imagess")
+  with text_column:
+    st.subheader("Cool project from cse154")
+    st.write(
+    """
+    Commodo sed egestas egestas fringilla phasellus faucibus scelerisque. Pulvinar mattis nunc sed blandit libero volutpat sed cras. Fringilla est ullamcorper eget nulla. Egestas sed tempus urna et pharetra pharetra massa massa. Nunc vel risus commodo viverra maecenas accumsan lacus. Justo donec enim diam vulputate ut pharetra. Metus aliquam eleifend mi in nulla posuere sollicitudin. Lobortis mattis aliquam faucibus purus. Massa tincidunt dui ut ornare lectus sit amet. Et molestie ac feugiat sed lectus vestibulum. Mattis ullamcorper velit sed ullamcorper. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Pulvinar sapien et ligula ullamcorper malesuada. Orci phasellus egestas tellus rutrum tellus pellentesque. Ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet. Etiam erat velit scelerisque in dictum non consectetur.
+    """
+    )
+    st.markdown("[Link](www.example.com)")
+
+# Interaction Demo
+st.divider()
+st.header("Beanie Baby Bot!")
+clicked = st.button("Click me")
+if clicked:
+  st.balloons()
+
+
+st.sidebar.markdown("# My website!")
+st.sidebar.markdown('''
+## Sections
+- [About Me](#about-me)
+- [Projects](#projects)
+- [Fun Buttons](#fun-buttons)
+''', unsafe_allow_html=True)
+
+st.sidebar.markdown(
+      """
+      ## Contact
+      - ebelz@cs.washington.edu
+      - cel: 206.xxx.xxxx
+      """
+)
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,92 +1,81 @@
 import streamlit as st
-import requests
-from streamlit_lottie import st_lottie
+from openai import OpenAI
+import openai
+import pinecone
+import os
+import json
+from dotenv import load_dotenv
 
+load_dotenv()
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')  # Replace with your api key
+PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
+PINECONE_API_ENV = "gcp-starter"
+MAX_CONTEXT = 5 # conversational memory window. First index is system call
+model_name = 'text-embedding-ada-002'
 
-st.set_page_config(page_title="Cool website" ,page_icon = ":fire:", layout="wide")
+if (not (OPENAI_API_KEY and PINECONE_API_ENV)):
+    OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+    PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
 
-def lottie_request(url):
-  res = requests.get(url)
-  if res.status_code != 200:
-    return None
-  return res.json()
-
-lottie_animation = lottie_request("https://lottie.host/063967a2-9475-494a-b25e-4be2af9e3d6f/z4iD50gHm8.json")
-
-st.header("Hi, Welcome to my website :wave:")
-st.divider()
-
-# About Me
-st.header("About Me")
-column1, column2 = st.columns(2)
-with column1:
-  st.write("Hi, my name is Elias I am a Senior at the University of Washington!")
-  st.markdown("""
-              #### Classes I am taking
-              - cse154
-              - cse154
-              - cse154
-              """)
-  st.write(
-    """
-    Commodo sed egestas egestas fringilla phasellus faucibus scelerisque. Pulvinar mattis nunc sed blandit libero volutpat sed cras. Fringilla est ullamcorper eget nulla. Egestas sed tempus urna et pharetra pharetra massa massa. Nunc vel risus commodo viverra maecenas accumsan lacus. Justo donec enim diam vulputate ut pharetra. Metus aliquam eleifend mi in nulla posuere sollicitudin. Lobortis mattis aliquam faucibus purus. Massa tincidunt dui ut ornare lectus sit amet. Et molestie ac feugiat sed lectus vestibulum. Mattis ullamcorper velit sed ullamcorper. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Pulvinar sapien et ligula ullamcorper malesuada. Orci phasellus egestas tellus rutrum tellus pellentesque. Ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet. Etiam erat velit scelerisque in dictum non consectetur.
-    """
-  )
-
-with column2:
-  st_lottie(lottie_animation, height=400)
-
-
-# Projects
-st.divider()
-with st.container():
-  st.header("Projects")
-  img_column, text_column = st.columns((1,2))
-  with img_column:
-    st.image("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fd2%2Ffa%2F99%2Fd2fa99e4c66da9df17f40474b62864b1.jpg&f=1&nofb=1&ipt=775293a832086fafb85491ec1d083db04c1457f052fa56baaa88f50a1ff2834b&ipo=imagess")
-  with text_column:
-    st.subheader("My dubhacks project")
-    st.write(
-    """
-    Commodo sed egestas egestas fringilla phasellus faucibus scelerisque. Pulvinar mattis nunc sed blandit libero volutpat sed cras. Fringilla est ullamcorper eget nulla. Egestas sed tempus urna et pharetra pharetra massa massa. Nunc vel risus commodo viverra maecenas accumsan lacus. Justo donec enim diam vulputate ut pharetra. Metus aliquam eleifend mi in nulla posuere sollicitudin. Lobortis mattis aliquam faucibus purus. Massa tincidunt dui ut ornare lectus sit amet. Et molestie ac feugiat sed lectus vestibulum. Mattis ullamcorper velit sed ullamcorper. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Pulvinar sapien et ligula ullamcorper malesuada. Orci phasellus egestas tellus rutrum tellus pellentesque. Ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet. Etiam erat velit scelerisque in dictum non consectetur.
-    """
-    )
-    st.markdown("[Link](www.example.com)")
-with st.container():
-  st.header("Projects")
-  img_column, text_column = st.columns((1,2))
-  with img_column:
-    st.image("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fd2%2Ffa%2F99%2Fd2fa99e4c66da9df17f40474b62864b1.jpg&f=1&nofb=1&ipt=775293a832086fafb85491ec1d083db04c1457f052fa56baaa88f50a1ff2834b&ipo=imagess")
-  with text_column:
-    st.subheader("Cool project from cse154")
-    st.write(
-    """
-    Commodo sed egestas egestas fringilla phasellus faucibus scelerisque. Pulvinar mattis nunc sed blandit libero volutpat sed cras. Fringilla est ullamcorper eget nulla. Egestas sed tempus urna et pharetra pharetra massa massa. Nunc vel risus commodo viverra maecenas accumsan lacus. Justo donec enim diam vulputate ut pharetra. Metus aliquam eleifend mi in nulla posuere sollicitudin. Lobortis mattis aliquam faucibus purus. Massa tincidunt dui ut ornare lectus sit amet. Et molestie ac feugiat sed lectus vestibulum. Mattis ullamcorper velit sed ullamcorper. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Pulvinar sapien et ligula ullamcorper malesuada. Orci phasellus egestas tellus rutrum tellus pellentesque. Ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet. Etiam erat velit scelerisque in dictum non consectetur.
-    """
-    )
-    st.markdown("[Link](www.example.com)")
-
-# Interaction Demo
-st.divider()
-st.header("Beanie Baby Bot!")
-clicked = st.button("Click me")
-if clicked:
-  st.balloons()
-
-
-st.sidebar.markdown("# My website!")
-st.sidebar.markdown('''
-## Sections
-- [About Me](#about-me)
-- [Projects](#projects)
-- [Fun Buttons](#fun-buttons)
-''', unsafe_allow_html=True)
-
-st.sidebar.markdown(
-      """
-      ## Contact
-      - ebelz@cs.washington.edu
-      - cel: 206.xxx.xxxx
-      """
+# initialize pinecone
+pinecone.init(
+    api_key=PINECONE_API_KEY,  # find at app.pinecone.io
+    environment=PINECONE_API_ENV  # next to api key in console
 )
 
+# Define the name of the index and the dimensionality of the embeddings
+index_name = "beanie-data"
+dimension = 1536 # opeanAI default
+
+index = pinecone.Index(index_name)
+
+#openai.api_key = OPENAI_API_KEY
+client = OpenAI(api_key=OPENAI_API_KEY)
+# openai.api_key = OPENAI_API_KEY
+
+
+# Search for beanie baby using Pinecone vector db
+def beanie_search(query):
+    # Embed question
+    response = client.embeddings.create(input=query, model=model_name)
+    xq = response.data[0].embedding;
+    res = index.query([xq], top_k=3, include_metadata=True)
+    beanies = ""
+    for match in res['matches']:
+        beanies+=(f"{match['metadata']['text']}")
+    return beanies
+
+st.title("Beanie Baby Bot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# communicating with the bot
+if prompt := st.chat_input("What is up?"):
+    beanie_req = beanie_search(prompt)
+    print(beanie_req)
+    st.session_state.messages.append({"role": "user", "content": prompt + "\n" + beanie_req })
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += (response.choices[0].delta.content or "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
